431d1ef7425aef53990cc08cb2dd4efac14e97fe
ninvit <ninvit@gmail.com>
2024-07-12 02:20:51 -0300

    feature: desafio lock para concorrencia de transacoes

--- src/main/java/app/repository/CartaoRepository.java
+++ src/main/java/app/repository/CartaoRepository.java
@@ -1,12 +1,15 @@
 package app.repository;
 
 import app.model.Cartao;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface CartaoRepository extends JpaRepository<Cartao, String> {
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     Optional<Cartao> findByNumeroCartao(String numeroCartao);
 }
